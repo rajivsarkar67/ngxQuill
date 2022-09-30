f227b537a09d261bca2360f93c6a29881c8e97fa
rajib_sarkar <rajib.sarkar@forgeahead.io>
2022-09-30 14:09:14 +0530

    made some progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill/public-api';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-import { Route, Routes } from '@angular/router';
-import {Router} from '@angular/router';
-
-import 'pdfmake/build/pdfmake';
-import { TitleStrategy } from '@angular/router';
-const pdfMake = window["pdfMake"];
-
-
 
 @Component({
   selector: 'app-root',
@@ -19,22 +9,19 @@ const pdfMake = window["pdfMake"];
 export class AppComponent {
   @ViewChild('htmlData') htmlData!: ElementRef;
   editorContent = '';
+  replacedContent = '';
+  myarr : number[] = [];
+  count = 0;
 
-  newestTry(){
-    console.log('newest try getting called');
-    let data: any = document.getElementsByClassName('ql-container')[0];
-    this.generatePdf(data);
+  addTextBox(){
+    this.editorContent = this.editorContent + '<span class="dynamicField">_______________</span>';
+    this.myarr.push(1);
   }
-
-  generatePdf(htmlContent:any){
-    html2canvas(htmlContent).then(canvas => {
-      let imgWidth = 290;
-      let imgHeight = (canvas.height * imgWidth)/canvas.width;
-      const contentDataURL = canvas.toDataURL('image/png');
-      let pdf = new jsPDF('p','mm','a4');
-      let position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-      pdf.save('demo.pdf');
-    })
+  replaceText(){
+    this.editorContent = this.editorContent.replace('_______________', this.replacedContent);
+  }
+  countTextBoxes(){
+    this.count = (this.editorContent.match(/_______________/g) || []).length;
+    console.log(this.count);
   }
 }
