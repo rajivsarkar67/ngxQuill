e8a6928034f07424d5abb8c05d4f55a0ada45764
rajib_sarkar <rajib.sarkar@forgeahead.io>
2022-09-30 10:51:31 +0530

    quill code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill/public-api';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { Route, Routes } from '@angular/router';
+import {Router} from '@angular/router';
+
+import 'pdfmake/build/pdfmake';
+import { TitleStrategy } from '@angular/router';
+const pdfMake = window["pdfMake"];
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngxQuill';
+  @ViewChild('htmlData') htmlData!: ElementRef;
+  editorContent = '';
+
+  newestTry(){
+    console.log('newest try getting called');
+    let data: any = document.getElementsByClassName('ql-container')[0];
+    this.generatePdf(data);
+  }
+
+  generatePdf(htmlContent:any){
+    html2canvas(htmlContent).then(canvas => {
+      let imgWidth = 290;
+      let imgHeight = (canvas.height * imgWidth)/canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png');
+      let pdf = new jsPDF('p','mm','a4');
+      let position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('demo.pdf');
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuillModule } from 'ngx-quill';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QuillModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+}
